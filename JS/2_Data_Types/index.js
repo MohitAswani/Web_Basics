6d58e47a5b6346aa517371b0e1d8e08e594f1b74
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-02-01 16:37:24 +0530

    Adding code

---
+++ JS/2_Data_Types/index.js
@@ -0,0 +1,70 @@
+console.log("Hello bitches")
+
+
+// Variables
+
+// Numbers
+var number1=69;
+var number2=420;
+var number3=69.420
+console.log(number1+number2);
+
+// Strings
+var str1="This is a string";
+var str2='This is another string';
+
+console.log(str1);
+
+// Objects
+var marks={
+    p1:45,
+    p2:50,
+    p3:100
+};  // this data type is like a map
+
+console.log(marks);
+console.log(marks.p1);
+
+// Booleans
+var bool1=true;
+var bool2=false;
+
+console.log(bool1==bool2)
+
+// Undefined 
+var und=undefined
+var u // every variable by default is undefined 
+
+console.log(und)
+console.log(u)
+
+// Null
+var nul=null  // undefined is different from null and undefined is when a variable is not defined and null is when a varialbe contains nothing
+
+console.log(nul)
+console.log(null)
+
+
+// Symbol
+Symbol('')
+
+
+// Arrays
+var arr=[1,"L",3,4,5]
+
+console.log(arr)
+console.log(arr[1])
+console.log(arr[0])
+console.log(arr[5]) // this will be undefined
+
+
+/* So javascript like other languages has 2 types of data types : 
+
+1) Primitive data types : numbers, string , boolean, null , undefined and symbol.
+
+Symbol is type of variable which when used gives us a unique value.
+
+2) Reference data : objects (like maps) and arrays
+
+*/
+
---
+++ JS/3_Operators/index.js
@@ -0,0 +1,14 @@
+var a=34;
+var b=56;
+
+// Arithematic ops 
+console.log("The value of a+b = ",a-b)
+
+// Assignment ops : same as rest of langs
+var c=b;
+c++;
+c+=b;
+c-=b;
+c/=b;
+c+=2;
+console.log(c);
