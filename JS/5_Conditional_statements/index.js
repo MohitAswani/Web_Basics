b7c1b6edb0d742970f2e11981ea32005ecc6b2dd
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-02-01 21:39:05 +0530

    Adding code

---
+++ JS/5_Conditional_statements/index.js
@@ -0,0 +1,28 @@
+
+// Order matters in JS
+
+var age=34;
+
+if(age>=18){
+    console.log("Baalik hai launda")
+}
+else{
+    console.log("Na baalik hai gandu")
+}
+
+
+var i=45;
+
+if(i%3==0){
+    console.log("Rem is 0")
+}
+else if(i%3==1){
+    console.log("Rem is 1")
+}
+else if(i%3==2){
+    console.log("Rem is 2")
+}
+else{
+    console.log("Number daal bc")
+}
+
