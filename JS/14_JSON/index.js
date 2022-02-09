f06cf28b5e56bc74665da7bd4b5b0991380aa6f7
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-02-09 18:19:20 +0530

    Adding code

--- JS/14_JSON/index.js
+++ JS/14_JSON/index.js
@@ -10,3 +10,9 @@ console.log(typeof jso);
 
 parsed=JSON.parse(jso)  // this will convert the string of JSON object back to JSON
 console.log(parsed)
+
+
+// We can also use `` to write strings in JS
+
+a= 69;
+console.log(`this is my ${a}`)  // ` can be used to include variables like this
